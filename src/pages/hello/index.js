51bed1cedad5646e4b3ba66aa6f15b20f111abe3
soratenchan <54834092+soratenchan@users.noreply.github.com>
2023-02-16 11:50:59 +0900

    commit

---
+++ src/pages/hello/index.js
@@ -0,0 +1,3 @@
+export default function Hello(second) {
+  return <div>Hello Page</div>;
+}
---
+++ src/pages/hello/world.js
@@ -0,0 +1,8 @@
+export default function World(second) {
+  return (
+    <div>
+      <img src="/next.svg" />
+      <p>World Page</p>
+    </div>
+  );
+}
